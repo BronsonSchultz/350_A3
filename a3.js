9702c148067709182689aae3e835c58a5095e2a0
Bronson <bronson@uofs-10-230-76-172.usask.ca>
2020-02-11 12:59:39 -0600

    added packageJSON and functionality

--- a3.js
+++ a3.js
@@ -4,29 +4,78 @@ CMPT 350 Assignment 3, javascript
 */
 
 const prompt = require("prompt");
+const express = require('express');
+const bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var port = process.env.PORT || 3000;
+const router = express.Router();
+router.get('/', function(req, res) {
+  res.json({message: 'API in Online!'});
+});
+
 console.log("Hello World");
 
+/**
+ * chatRoom object allowing users to communicate and save
+ * conversation
+ */
 class ChatRoom {
   constructor() {
       this.numUsers = 0;
       this.logger = null;
   }
 
-  getNumUsers() {
+
+/**
+ * return current number of users in the chatroom
+ * @return {int} numUsers
+ */
+ getNumUsers() {
     return this.numUsers;
   }
 
-  setNumUsers(i) {
+
+/**
+ * set current users in chatroom
+ * @param {int} i the new value for numUsers
+ */
+ setNumUsers(i) {
     this.numUsers = i;
   }
 
+
+/**
+ * prompt the user for new message
+ * @throws will throw error if error occurs asking for prompt
+ *  */
+  askForMessage(){
+    prompt.start();
+    prompt.get(['message'], function (err, result){
+
+    if (err) {
+        return console.log(err);
+    }
+
+    var d = new Date().toString().substring(16,21);
+
+    console.log(d + " > " + result.message);
+    });
+  }
+
+
+
 }
+
 
 let c = new ChatRoom();
 console.log(c.getNumUsers());
 
-prompt.start();
-prompt.get(['users'], function (err, result){
-  if (err) {return console.log(err); }
-  console.log('Users' + result.users);
-})
+
+app.use('/', router);
+app.listen(port);
+console.log('server online at: ' + port);
+
+c.askForMessage();
