4eac652bf2e6cd87140130e71fe8e0fdfba9a1f3
Bronson Schultz <bcs269@peon10.usask.ca>
2020-02-12 12:16:41 -0600

    deleted uneeded files and started front end

--- react_proj/src/index.js
+++ react_proj/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class MessageBox extends React.Component {
+  render() {
+    return (
+      <form>
+        <h1>Hello</h1>
+        <p>Enter and message:</p>
+        <input type="text" />
+      </form>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<MessageBox />, document.getElementById('root'))
